9c64506f0334091c574e290ccbed168eb3aee7e8
Patryk A. Wertka <spartanpageagain@gmail.com>
2014-11-29 23:35:58 +0100

    Brand new AsyncToken class. In its own sourcefile B|.
    Signed-off-by: Patryk A. Wertka <spartanpageagain@gmail.com>

---
+++ Server/AsyncToken.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+//TODO: Xml docs.
+namespace TastySoap{
+    public class AsyncToken : IDisposable{
+        public Socket Connection{ get; private set; }
+        public Stack<Byte> Stack{ get; private set; }
+
+        public AsyncToken(Socket connection, Int32 bufferSize){
+            Connection = connection;
+            Stack = new Stack<Byte>(bufferSize);
+        }
+
+        public void addToStack(SocketAsyncEventArgs args){
+            //TODO: Find a way to append to stack and use it.
+            for(int i = args.Offset; i < args.Buffer.Length; ++i)
+                Stack.Push(args.Buffer[i]);
+        }
+
+        override public void Dispose(){
+            try{
+                Connection.Shutdown(SocketShutdown.Send);
+            } 
+            catch(Exception){ /* Client closed socket? Who cares! */ } 
+            finally {
+                Connection.Close();
+            }
+        }
+    }
+}
