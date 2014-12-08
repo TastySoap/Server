ff42b26722f449bbad60e50cbf83c2ec02b2f3a8
Patryk A. Wertka <spartanpageagain@gmail.com>
2014-12-08 13:49:01 +0100

    Program.ms now starts the server on local ip
    Signed-off-by: Patryk A. Wertka <spartanpageagain@gmail.com>

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,6 +3,16 @@ using System.Net;
 namespace TastySoap{
     class Program{
         static void Main(string[] args){
+            var addr = Dns.GetHostEntry("127.0.0.1").AddressList[0];
+            var port = 1337;
+            var ipEndpoint = new IPEndPoint(addr, port);
+
+            var server = new AsyncServer(ipEndpoint, 1, 1024);
+
+            server.Start();
+
+            while(true)
+                ;
         }
     }
 }
